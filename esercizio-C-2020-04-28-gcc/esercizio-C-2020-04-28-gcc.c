eaab3608342b9e4da3fad572da2105afb32fba69
utente <utente@debian10.units.it>
2020-05-25 22:09:00 +0200

    completo

--- esercizio-C-2020-04-28-gcc/esercizio-C-2020-04-28-gcc.c
+++ esercizio-C-2020-04-28-gcc/esercizio-C-2020-04-28-gcc.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <sys/inotify.h>
+
+#include <fcntl.h>
 #include <errno.h>
+#include <semaphore.h>
+#include <dirent.h>
+
+#define CHECK_ERR(a, msg){if((a) == -1){perror(msg); exit(-1); } }
 
 #define BUF_LEN 4096
+
+sem_t * semaphore_pid_list;
+
+int pid_list_len=1;
+pid_t * pid_list;
 
 char buf[BUF_LEN] __attribute__ ((aligned(__alignof__(struct inotify_event))));
 
@@ -22,33 +34,8 @@ int check_file_exist(char * fname) {
 	}
 }
 
-int inotify_modify(struct inotify_event *i){
 
-
-	int modify =-1;
-
-    if (i->cookie > 0)
-        printf("cookie=%4d \n", i->cookie);
-
-    // The  name  field is present only when an event is returned for a file
-    // inside a watched directory; it identifies the filename within the watched directory.
-    // This filename is null-terminated .....
-
-    if (i->len > 0)
-        printf("file name = %s \n", i->name);
-    else
-    	printf("*no file name* \n"); // event refers to watched directory
-
-    // IN_CLOSE_NOWRITE  File or directory not opened for writing was closed.
-    if (i->mask & IN_CLOSE_WRITE){
-    	system("gcc src/hello_world.c -o hello");
-    	modify = 0;
-    }
-    return modify;
-
-}
-
-
+//il programma riceve in un suo signal handler il segnale SIGCHLD
 void signal_handler(int signum) {
 	// riceviamo SIGCHLD: Child stopped or terminated
 
@@ -58,93 +45,91 @@ void signal_handler(int signum) {
 
 	printf("[parent] signal handler: processo %u è terminato, ora termino anche io\n", child_pid);
 
+	if (sem_wait(semaphore_pid_list) == -1) {
+			perror("sem_wait");
+			exit(EXIT_FAILURE);
+		}
+
+	for(int i=0; i< pid_list_len;i++){
+
+		if(pid_list[i] == child_pid){
+			pid_list[i] = 0;
+			break;
+		}
+	}
+
+	if (sem_post(semaphore_pid_list) == -1) {
+			perror("sem_post");
+			exit(EXIT_FAILURE);
+		}
 }
 
-int main(){
+int main(int argc, char * argv[]){
 
-	if(check_file_exist("src") == 0)
-		mkdir("src", ACCESSPERMS);
+	if(check_file_exist("./src") == 0)
+		mkdir("./src", ACCESSPERMS);
 
 //dentro src, se non esiste già, crea il file hello_world.c contenente il codice sorgente del programma
 //che scrive "hello world" su stdout.
 //uso funzione PIPE
 
 
-	int fd_hello = open("src/hello_world.c", O_WRONLY | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR);
+	int fd_hello = open("./src/hello_world.c", O_WRONLY | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR);
 
-	if(fd_hello == -1) { // errore!
-		perror("open()");
-		exit(EXIT_FAILURE);
-	}
+	CHECK_ERR(fd_hello,"open()")
 
-	FILE* hello = fdopen(fd_hello, "w");
-	if(hello == NULL) { // errore!
-		perror("fdopen()");
-		exit(EXIT_FAILURE);
-	}
+	int codice_len = -1;
 
-	int fd_hello_orig = open("/home/utente/git/exOpSys/000hello-world/hello_world.c", O_WRONLY | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR);
+	char * codice;
+	codice = "#include <stdio.h>\n int main(){\nprintf(\"Hello world!\\n\");\n return 0;\n}";
 
-	if(fd_hello_orig == -1) { // errore!
-		perror("open()");
-		exit(EXIT_FAILURE);
-	}
+	codice_len = strlen(codice);
+	int writin;
 
-	FILE* hello_orig = fdopen(fd_hello, "w");
-	if(hello_orig == NULL) { // errore!
-		perror("fdopen()");
-		exit(EXIT_FAILURE);
-	}
-	int ch;
+	writin= write(fd_hello, codice, codice_len);
 
-	while((ch = fgetc(hello_orig)) != EOF){
-	      fputc(ch, hello);
-	}
+	CHECK_ERR(writin,"write()")
 
-	close(fd_hello_orig);
-	fclose(hello);
+	close(fd_hello);
 
 //dentro src, crea un file output.txt; se il file esiste già, lo "tronca" a dimensione 0
 
-	int fd_out = open("src/output.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	int fd_out = open("./src/output.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if(fd_out == -1) { // errore!
-		perror("open()");
-		exit(EXIT_FAILURE);
-	}
-
-//il programma riceve in un suo signal handler il segnale SIGCHLD
-
-
+	CHECK_ERR(fd_out,"open()")
 
 //entra in un ciclo senza fine:
 
-	pid_t * pid_list;
-	int contapid=1;
-
 	pid_list = malloc(sizeof(pid_t));
 
-	while(1){
-
-//monitora il file hello_world.c e ogni volta che viene notificato un evento di modifica del file
-
 	int fd_notify = inotify_init();
-	fd_notify = inotify_add_watch(fd_notify, "src/hello_world.c", IN_CLOSE_WRITE);
 
-	if(fd_notify==-1){
-		perror("inotify_add_watch");
-		exit(EXIT_FAILURE);
-	}
+	CHECK_ERR(fd_notify,"inotify_init()")
 
+	int watch_descriptor;
+
+	watch_descriptor = inotify_add_watch(fd_notify, "./src/hello_world.c", IN_MODIFY);
+
+	CHECK_ERR(watch_descriptor,"inotify_add_watch()")
+
+
+	if ((semaphore_pid_list = sem_open("/semaphore", O_CREAT, 00600, 1)) == SEM_FAILED) {
+			perror("sem_open");
+			exit(EXIT_FAILURE);
+		}
+	int num_bytes_read;
+
+	while(1){
+//monitora il file hello_world.c e ogni volta che viene notificato un evento di modifica del file
 //(quando il file viene modificato da un editor quale pico, gedit o altro) il programma esegue
 //il compilatore gcc per compilare il codice sorgente contenuto nel file hello_world.c e produrre
 //un file eseguibile di nome hello (gcc hello_world.c -o hello).
-	int num_bytes_read;
 
 	num_bytes_read = read(fd_notify, buf, BUF_LEN);
+
 	if (num_bytes_read == 0) {
-		printf("read() from inotify fd returned 0!");
-		exit(EXIT_FAILURE);
+			printf("read() from inotify fd returned 0!");
+			exit(EXIT_FAILURE);
 	}
 
 	if (num_bytes_read == -1) {
@@ -158,85 +143,87 @@ int main(){
 		}
 	}
 
-	struct inotify_event *event;
-	int res;
+	int status=-1;
 
-	for (char * p = buf; p < buf + num_bytes_read; ) {
-		event = (struct inotify_event *) p;
+	status = system("gcc ./src/hello_world.c -o hello");
 
-		res = inotify_modify(event);
+		if(WIFEXITED(status)){
+			//result =  WEXITSTATUS(status);
+			pid_t child_pid;
 
-		p += sizeof(struct inotify_event) + event->len;
-		// event->len is length of (optional) file name
-	}
-
-	if(res == 0){
-
-		switch(fork()){
-
-		case 0:
-			pid_list[contapid-1] = getpid();
-			int pfd[2];
-
-			if (pipe(pfd) == -1) {
-				perror("problema con pipe");
-
-				exit(EXIT_FAILURE);
-			}
-			if (dup2(pfd[0], STDIN_FILENO) == -1) {
-				perror("problema con dup2");
-				exit(EXIT_FAILURE);
-			}
-			close(pfd[0]);
-
-			if (dup2(pfd[1], STDOUT_FILENO) == -1) {
-				perror("problema con dup2");
-				exit(EXIT_FAILURE);
-			}
-			close(pfd[1]);
-
-			execlp("hello", "hello", (char * ) NULL);
-
-			perror("problema con execlp(1)");
-
-			exit(EXIT_SUCCESS);
-			break;
-		case -1:
-			break;
-			exit(-1);
-		default:
-			pid_list = realloc(pid_list, contapid*sizeof(pid_t));
-			if(pid_list == NULL){
-				perror("malloc()");
-				exit(-1);
-			}
-			contapid++;
-
-			break;
-			exit(EXIT_SUCCESS);
-
-	}
-		if (signal(SIGCHLD, signal_handler) == SIG_ERR) {
-			perror("signal");
-			exit(EXIT_FAILURE);
-		}
-
-	}
 //*seconda parte dell'esercizio:*
 //se gcc ha successo, il programma invoca fork() ed il processo figlio esegue il programma hello
 //appena compilato dove però l'output di hello va "redirezionato" sul file output.txt
 //(quello creato all'avvio del programma, più sopra).
 
+			switch(child_pid = fork()){
+
+			case 0:
+				if (dup2(fd_out, STDOUT_FILENO) == -1) {
+					perror("problema con dup2");
+					exit(EXIT_FAILURE);
+				}
+
+				close(fd_out);
+
+				execve("./hello", (char *)NULL, (char *)NULL);
+
+				perror("execve");
+
+				exit(EXIT_FAILURE);
+				break;
+			case -1:
+				break;
+				exit(-1);
+			default:
+
+				if (sem_wait(semaphore_pid_list) == -1) {
+							perror("sem_wait");
+							exit(EXIT_FAILURE);
+						}
+//il programma non aspetta la terminazione del processo figlio ma "prende nota" del PID
+//del processo figlio aggiungendolo in coda ad un array dinamico chiamato pid_list.
+				pid_list = realloc(pid_list, pid_list_len*sizeof(pid_t));
+
+				if(pid_list == NULL){
+					perror("realloc()");
+					exit(-1);
+				}
+
+				pid_list[pid_list_len-1] = child_pid;
+				pid_list_len++;
+
+				if (sem_post(semaphore_pid_list) == -1) {
+						perror("sem_post");
+						exit(EXIT_FAILURE);
+					}
+				break;
+				exit(EXIT_SUCCESS);
+
+			}
+			if (signal(SIGCHLD, signal_handler) == SIG_ERR) {
+						perror("signal");
+						exit(EXIT_FAILURE);
+			}
+
+		}
+}
+
+
+
+
+
+
+
 
 
 //
-//il programma non aspetta la terminazione del processo figlio ma "prende nota" del PID
-//del processo figlio aggiungendolo in coda ad un array dinamico chiamato pid_list.
 
+return 0;
 
 	}
 
-}
+
 
 
 
--- esercizio-C-2020-04-28-gcc/src/hello_world.c
+++ esercizio-C-2020-04-28-gcc/src/hello_world.c
@@ -1,2 +1,5 @@
-figo
-
+#include <stdio.h>
+ int main(){
+printf("Hello world!\n");
+ return 0;
+}
